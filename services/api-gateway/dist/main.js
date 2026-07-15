8c1948b14403c032b77d242afd6753d1cbd0e41c
reddyshekharc <reddyshekharc55@gmail.com>
2026-07-15 21:15:18 +0530

    Deleted dist folder

--- services/api-gateway/dist/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "./app.js";
-import { config, getSafeConfigForLog } from "./config.js";
-const app = createApp();
-const port = config.PORT;
-console.log("Gateway config loaded", getSafeConfigForLog());
-app.listen(port, () => {
-    console.log(`api-gateway running on port ${port}`);
-});
